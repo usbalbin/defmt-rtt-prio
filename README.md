8a10a3b583f522cda51040e5b0151092fd4774de
Albin Hedman <albin9604@gmail.com>
2026-03-19 00:52:34 +0100

    Add support for more devices by making prio bits configurable using cargo features

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > Transmit [`defmt`] log messages over the RTT (Real-Time Transfer) protocol without blocking
 
-This is based on defmt-rtt from [knurling-rs/defmt](https://github.com/knurling-rs/defmt).However `defmt-rtt-prio` avoids any critical sections by exploiting the fact that interrupts of the same priority can not interrupt each other. We setup one RTT UP channel per NVIC priority level. By mapping each priority to its own RTT channel, we can guarantee that there will be no problems.
+This is based on defmt-rtt from [knurling-rs/defmt](https://github.com/knurling-rs/defmt). However `defmt-rtt-prio` avoids any critical sections by exploiting the fact that interrupts of the same priority can not interrupt each other. We setup one RTT UP channel per NVIC priority level. By mapping each priority to its own RTT channel, we can guarantee that there will be no problems.
 
 NOTE this crate currently only works with Cortex-M devices
 
@@ -22,9 +22,24 @@ use defmt_rtt_prio as _;
 
 For more details about the defmt framework check the book at <https://defmt.ferrous-systems.com>.
 
-## Memory use
+## Configuration
+
+### Priority bits
+
+You **must** select the number of interrupt priority bits your chip implements by enabling exactly one `prio_bits_X` feature:
+
+```toml
+[dependencies]
+defmt-rtt-prio = { version = "0.1", features = ["prio_bits_4"] }
+```
+
+Common values: Cortex-M0/M0+ typically use 2, Cortex-M3/M4/M7 typically use 3 or 4. Check your chip's reference manual.
+
+### Memory use
 
 When in a tight memory situation and logging over RTT, the buffer size (default: 1024 bytes) can be configured with the `DEFMT_RTT_BUFFER_SIZE` environment variable. Use a power of 2 for best performance.
+
+The number of RTT UP channels (default: 2^*prio_bits* + 2) can be configured with the `DEFMT_RTT_UP_CHANNELS` environment variable. Reducing this saves memory but silences the most urgent interrupt priorities.
 
 ## License
 
--- build.rs
+++ build.rs
@@ -1,9 +1,76 @@
-use std::{env, path::PathBuf};
+#[cfg(not(any(
+    feature = "prio_bits_2",
+    feature = "prio_bits_3",
+    feature = "prio_bits_4",
+    feature = "prio_bits_5",
+    feature = "prio_bits_6",
+    feature = "prio_bits_7",
+    feature = "prio_bits_8"
+)))]
+fn main() {}
 
+#[cfg(any(
+    feature = "prio_bits_2",
+    feature = "prio_bits_3",
+    feature = "prio_bits_4",
+    feature = "prio_bits_5",
+    feature = "prio_bits_6",
+    feature = "prio_bits_7",
+    feature = "prio_bits_8"
+))]
 fn main() {
+    use std::{env, path::PathBuf};
+
+    let target = env::var("TARGET").unwrap();
+
     println!("cargo:rerun-if-env-changed=DEFMT_RTT_BUFFER_SIZE");
     println!("cargo:rerun-if-env-changed=DEFMT_RTT_UP_CHANNELS");
     println!("cargo:rerun-if-changed=build.rs");
+
+    println!("cargo:rustc-check-cfg=cfg(armv6m)");
+    println!("cargo:rustc-check-cfg=cfg(armv7m)");
+    println!("cargo:rustc-check-cfg=cfg(armv7em)");
+    println!("cargo:rustc-check-cfg=cfg(armv8m)");
+    println!("cargo:rustc-check-cfg=cfg(armv8m_base)");
+    println!("cargo:rustc-check-cfg=cfg(armv8m_main)");
+    println!("cargo:rustc-check-cfg=cfg(cortex_m)");
+    println!("cargo:rustc-check-cfg=cfg(has_fpu)");
+    println!("cargo:rustc-check-cfg=cfg(native)");
+
+    if target.starts_with("thumbv6m-") {
+        println!("cargo:rustc-cfg=cortex_m");
+        println!("cargo:rustc-cfg=armv6m");
+    } else if target.starts_with("thumbv7m-") {
+        println!("cargo:rustc-cfg=cortex_m");
+        println!("cargo:rustc-cfg=armv7m");
+    } else if target.starts_with("thumbv7em-") {
+        println!("cargo:rustc-cfg=cortex_m");
+        println!("cargo:rustc-cfg=armv7m");
+        println!("cargo:rustc-cfg=armv7em"); // (not currently used)
+    } else if target.starts_with("thumbv8m.base") {
+        println!("cargo:rustc-cfg=cortex_m");
+        println!("cargo:rustc-cfg=armv8m");
+        println!("cargo:rustc-cfg=armv8m_base");
+    } else if target.starts_with("thumbv8m.main") {
+        println!("cargo:rustc-cfg=cortex_m");
+        println!("cargo:rustc-cfg=armv8m");
+        println!("cargo:rustc-cfg=armv8m_main");
+    }
+
+    #[cfg(feature = "prio_bits_2")]
+    let prio_bits = 2;
+    #[cfg(feature = "prio_bits_3")]
+    let prio_bits = 3;
+    #[cfg(feature = "prio_bits_4")]
+    let prio_bits = 4;
+    #[cfg(feature = "prio_bits_5")]
+    let prio_bits = 5;
+    #[cfg(feature = "prio_bits_6")]
+    let prio_bits = 6;
+    #[cfg(feature = "prio_bits_7")]
+    let prio_bits = 7;
+    #[cfg(feature = "prio_bits_8")]
+    let prio_bits = 8;
 
     let size = env::var("DEFMT_RTT_BUFFER_SIZE")
         .map(|s| {
@@ -17,7 +84,7 @@ fn main() {
             s.parse()
                 .expect("could not parse DEFMT_RTT_UP_CHANNELS as usize")
         })
-        .unwrap_or(17_usize);
+        .unwrap_or((1 << prio_bits) + 2);
 
     let out_dir_path = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let out_file_path = out_dir_path.join("consts.rs");
@@ -31,11 +98,15 @@ fn main() {
             /// Use a power of 2 for best performance.
             pub(crate) const BUF_SIZE: usize = {size};
 
-            /// Number of RTT UP channels (default: 17).
+            /// Number of RTT UP channels (default: 2^PRIO_BITS + 2).
             ///
             /// Can be customized by setting the `DEFMT_RTT_UP_CHANNELS` environment variable.
-            /// This should typically be 2^(NVIC priority bits) + 1
             pub(crate) const UP_CHANNELS: usize = {up_channels};
+
+            /// Number of priority bits
+            ///
+            /// Can be customized by setting `prio_bits_X` features
+            pub(crate) const PRIO_BITS: usize = {prio_bits};
             ",
         ),
     )
